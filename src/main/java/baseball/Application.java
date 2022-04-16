c858db9e8dc3d704f17a3dc66f24bbd8aba5892e
Yeseul-Kwon <ye_seul@kakao.com>
2022-04-17 01:34:26 +0900

    FEAT : Baseball Game Main
    (기능 5) 게임의 완전종료 여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,34 @@
 package baseball;
 
+import baseball.model.Game;
+import baseball.ui.Input;
+import baseball.ui.Output;
+
 public class Application {
+    public static final int MAX_LENGTH = 3;
+
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        Application application = new Application();
+        Boolean isReplay = true;
+        Game game = new Game();
+        while (isReplay) {
+            game.gameStart(MAX_LENGTH);
+            try {
+                isReplay = application.isValidInput(Input.receiveReplayInput());
+            } catch (IllegalArgumentException e) {
+                System.exit(0);
+            }
+        }
+    }
+
+    private Boolean isValidInput(String input) throws IllegalArgumentException {
+        if (!(input.equals("1") || input.equals("2"))) {
+            Output.printExceptionMessage(Output.REPLAY_INPUT_FORMAT_EXCEPTION_MESSAGE);
+            throw new IllegalArgumentException();
+        }
+        if (input.equals("1")) {
+            return true;
+        }
+        return false;
     }
 }
