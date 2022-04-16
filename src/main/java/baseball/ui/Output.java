7a00c2c8f04347d5d8c8f9274ea1468dfa55fa56
Yeseul-Kwon <ye_seul@kakao.com>
2022-04-17 01:29:08 +0900

    FEAT : Output UI
    (기능 2) 플레이어의 숫자 입력 받기 UI
    (기능 3) 플레이어 입력값과 정답을 비교하여 힌트 출력 UI
    (기능 4) 모든 숫자와 자리가 일치하면 게임 종료 UI
    (기능 5) 게임의 완전 종료 여부 확인 UI
    (기능 6) 모든 입력 시 잘못된 값을 입력할 경우 Exception UI

---
+++ src/main/java/baseball/ui/Output.java
@@ -0,0 +1,36 @@
+package baseball.ui;
+
+import baseball.Application;
+
+public class Output {
+    private static final String REQUEST_NUMBER_INPUT_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String REQUEST_REPLAY_INPUT_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
+    private static final String CORRECT_ANSWER_AND_EXIT_MESSAGE = "개의 숫자를 모두 맞히셨습니다! 게임 종료.";
+    public static final String BASEBALL_SIZE_EXCEPTION_MESSAGE = "자리 수를 입력해야 합니다.";
+    public static final String BASEBALL_DUPLICATE_EXCEPTION_MESSAGE = "중복된 숫자가 존재합니다.";
+    public static final String INPUT_NUMBER_FORMAT_EXCEPTION_MESSAGE = "1부터 9까지의 숫자만 입력 가능합니다.";
+    public static final String REPLAY_INPUT_FORMAT_EXCEPTION_MESSAGE = "1 또는 2만 입력 가능합니다.";
+    public static final String RESULT_NOTHING_MESSAGE = "낫싱";
+    public static final String RESULT_STRIKE_MESSAGE = "스트라이크 ";
+    public static final String RESULT_BALL_MESSAGE = "볼 ";
+
+    public static void printRequestNumberInput() {
+        System.out.print(REQUEST_NUMBER_INPUT_MESSAGE);
+    }
+
+    public static void printRequestReplayInput() {
+        System.out.println(REQUEST_REPLAY_INPUT_MESSAGE);
+    }
+
+    public static void printBaseballGameResult(String result) {
+        System.out.println(result);
+    }
+
+    public static void printCorrectAnswer() {
+        System.out.println(Application.MAX_LENGTH + CORRECT_ANSWER_AND_EXIT_MESSAGE);
+    }
+
+    public static void printExceptionMessage(String exceptionMessage) {
+        System.out.println(exceptionMessage);
+    }
+}
