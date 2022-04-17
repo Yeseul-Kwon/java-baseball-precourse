e5ef410246df4efb253f721849437ed408c5b632
Yeseul-Kwon <ye_seul@kakao.com>
2022-04-17 17:59:24 +0900

    REFACTOR : Baseball Game Main
    * 수정 : 공통함수 사용 및 변수명
    (기능 5) 게임의 완전종료 여부 확인

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,18 +1,19 @@
 package baseball;
 
+import baseball.model.CommonFunction;
 import baseball.model.Game;
 import baseball.ui.Input;
 import baseball.ui.Output;
 
 public class Application {
-    public static final int MAX_LENGTH = 3;
+    public static final int NUMBER_OF_DIGITS = 3;
 
     public static void main(String[] args) {
         Application application = new Application();
+        Game game = new Game(NUMBER_OF_DIGITS);
         Boolean isReplay = true;
-        Game game = new Game();
         while (isReplay) {
-            game.gameStart(MAX_LENGTH);
+            game.gameStart();
             try {
                 isReplay = application.isValidInput(Input.receiveReplayInput());
             } catch (IllegalArgumentException e) {
@@ -26,9 +27,6 @@ public class Application {
             Output.printExceptionMessage(Output.REPLAY_INPUT_FORMAT_EXCEPTION_MESSAGE);
             throw new IllegalArgumentException();
         }
-        if (input.equals("1")) {
-            return true;
-        }
-        return false;
+        return (CommonFunction.compareNumber(Integer.parseInt(input), 1));
     }
 }
