aeed9029628c73d4996964e43598d868f41e3616
Yeseul-Kwon <ye_seul@kakao.com>
2022-04-17 18:08:40 +0900

    REFACTOR : 입력값과 정답을 비교하며 게임 진행
    * 수정 : 공통함수 사용 및 중복되는 파라미터 삭제
    (기능 3) 플레이어 입력값과 정답을 비교하여 힌트 출력 반복
    (기능 4) 모든 숫자와 자리가 일치하면 게임 종료

--- src/main/java/baseball/model/Game.java
+++ src/main/java/baseball/model/Game.java
@@ -6,73 +6,86 @@ import baseball.ui.Output;
 import java.util.ArrayList;
 
 public class Game {
+    private int NUMBER_OF_DIGITS;
 
-    public void gameStart(int length) {
+    private ArrayList<Integer> rightAnswer;
+    private ArrayList<Integer> inputAnswer;
+
+    public Game() {
+    }
+
+    public Game(int numberOfDigits) {
+        this.NUMBER_OF_DIGITS = numberOfDigits;
+    }
+
+    public void gameStart() {
         Opponent opponent = new Opponent();
-        ArrayList<Integer> rightAnswer = opponent.getRightAnswer(length);
-        Boolean isIncorrect = true;
-        while (isIncorrect) {
-            String input = Input.receiveNumberInput();
-            Player player = new Player(input);
-            ArrayList<Integer> inputAnswer = player.getInputAnswer(input);
-            isIncorrect = gamePlay(rightAnswer, inputAnswer, length);
+        rightAnswer = opponent.getRightAnswer(NUMBER_OF_DIGITS);
+
+        Boolean isCorrect = false;
+        while (!isCorrect) {
+            Player player = new Player(Input.receiveNumberInput());
+            inputAnswer = player.getInputAnswer();
+            isCorrect = gamePlay();
         }
     }
 
-    private Boolean gamePlay(ArrayList<Integer> rightAnswer, ArrayList<Integer> inputAnswer, int length) {
-        int ball = getBall(rightAnswer, inputAnswer, length);
-        int strike = getStrike(rightAnswer, inputAnswer, length);
+    private Boolean gamePlay() {
+        int ball = getBall();
+        int strike = getStrike();
 
         String gameResult = getGameResult(ball, Output.RESULT_BALL_MESSAGE) + getGameResult(strike, Output.RESULT_STRIKE_MESSAGE);
         if (gameResult.equals("")) {
             gameResult = Output.RESULT_NOTHING_MESSAGE;
         }
         Output.printBaseballGameResult(gameResult);
-        return isCorrect(strike, length);
+        return isCorrect(strike);
     }
 
-    private int getBall(ArrayList<Integer> rightAnswer, ArrayList<Integer> inputAnswer, int length) {
+    private int getBall() {
         int ball = 0;
-        for (int i = 0; i < length; i++) {
-            ball += (compareValue(rightAnswer, inputAnswer.get(i)) - compareLocationAndValue(rightAnswer.get(i), inputAnswer.get(i)));
+        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
+            ball += (compareValue(rightAnswer, inputAnswer.get(i)) - compareSameLocationValue(rightAnswer.get(i), inputAnswer.get(i)));
         }
         return ball;
     }
 
-    private int getStrike(ArrayList<Integer> rightAnswer, ArrayList<Integer> inputAnswer, int length) {
+    private int getStrike() {
         int strike = 0;
-        for (int i = 0; i < length; i++) {
-            strike += compareLocationAndValue(rightAnswer.get(i), inputAnswer.get(i));
+        for (int i = 0; i < NUMBER_OF_DIGITS; i++) {
+            strike += compareSameLocationValue(rightAnswer.get(i), inputAnswer.get(i));
         }
         return strike;
     }
 
-    public int compareValue(ArrayList<Integer> target, int value) {
-        if (CommonFunction.overlapCheck(target, value)) {
+    private int compareValue(ArrayList<Integer> target, int value) {
+        ArrayList<Integer> targetList = new ArrayList<>(target);
+        targetList.add(value);
+        if (CommonFunction.overlapCheck(targetList)) {
             return 1;
         }
         return 0;
     }
 
-    public int compareLocationAndValue(int rightAnswer, int inputAnswer) {
-        if (rightAnswer == inputAnswer) {
+    private int compareSameLocationValue(int rightAnswer, int inputAnswer) {
+        if (CommonFunction.compareNumber(rightAnswer, inputAnswer)) {
             return 1;
         }
         return 0;
     }
 
-    public String getGameResult(int ballOrStrike, String message) {
+    private String getGameResult(int ballOrStrike, String message) {
         if (ballOrStrike > 0) {
             return ballOrStrike + message;
         }
         return "";
     }
 
-    public Boolean isCorrect(int strike, int length) {
-        if (strike == length) {
+    private Boolean isCorrect(int strike) {
+        if (CommonFunction.compareNumber(strike, NUMBER_OF_DIGITS)) {
             Output.printCorrectAnswer();
-            return false;
+            return true;
         }
-        return true;
+        return false;
     }
 }
