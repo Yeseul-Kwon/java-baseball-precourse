1ffae71413266b51ca1c57450e1ead53e4332635
Yeseul-Kwon <ye_seul@kakao.com>
2022-04-17 18:17:49 +0900

    REFACTOR : 입력값 유효성 체크 및 형변환
    * 수정 : 공통함수 사용 및 불필요한 파라미터 삭제
    (기능 2) 플레이어의 숫자 입력 시 유효성체크 및 형변환

--- src/main/java/baseball/model/Player.java
+++ src/main/java/baseball/model/Player.java
@@ -4,47 +4,48 @@ import baseball.Application;
 import baseball.ui.Output;
 
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Player {
+    private String input;
     private ArrayList<Integer> typeConvertedAnswer;
+
+    public Player(){}
 
     public Player(String input) {
         try {
-            isValidInputFormat(input);
-            isValidInputSize(input);
-            isValidInputDuplicate(input);
+            this.input = input;
+            this.typeConvertedAnswer = new ArrayList<>();
+            isValidInputFormat();
+            isValidInputSize();
+            isValidInputDuplicate();
         } catch (IllegalArgumentException e) {
             System.exit(0);
         }
     }
 
-    public ArrayList<Integer> getInputAnswer(String input) {
-        typeConvertedAnswer = new ArrayList<Integer>();
+    public ArrayList<Integer> getInputAnswer() {
         for (int i = 0; i < input.length(); i++) {
             typeConvertedAnswer.add((input.charAt(i) - 48));
         }
         return typeConvertedAnswer;
     }
 
-    private void isValidInputFormat(String input) throws IllegalArgumentException {
+    private void isValidInputFormat() throws IllegalArgumentException {
         if (!input.matches("^[1-9]*?")) {
             Output.printExceptionMessage(Output.INPUT_NUMBER_FORMAT_EXCEPTION_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
 
-    private void isValidInputSize(String input) throws IllegalArgumentException {
-        if (input.length() != Application.MAX_LENGTH) {
-            Output.printExceptionMessage(Application.MAX_LENGTH + Output.BASEBALL_SIZE_EXCEPTION_MESSAGE);
+    private void isValidInputSize() throws IllegalArgumentException {
+        if (input.length() != Application.NUMBER_OF_DIGITS) {
+            Output.printExceptionMessage(Application.NUMBER_OF_DIGITS + Output.BASEBALL_SIZE_EXCEPTION_MESSAGE);
             throw new IllegalArgumentException();
         }
     }
 
-    private void isValidInputDuplicate(String input) throws IllegalArgumentException {
-        Set<Integer> deduplicatedInput = new HashSet<>(getInputAnswer(input));
-        if (deduplicatedInput.size() != input.length()) {
+    private void isValidInputDuplicate() throws IllegalArgumentException {
+        if (CommonFunction.overlapCheck(getInputAnswer())) {
             Output.printExceptionMessage(Output.BASEBALL_DUPLICATE_EXCEPTION_MESSAGE);
             throw new IllegalArgumentException();
         }
